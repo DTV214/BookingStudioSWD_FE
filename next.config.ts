3b20a91b920356f2d0c0fd0065fc9da5f11a0cae
dtvgaming214 <dtvgaming214@gmail.com>
2025-10-16 15:03:34 +0700

    [UPDATE] Update next.config.ts to add image of db

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: __dirname, // chỉ định thư mục hiện tại là root
+    root: __dirname,
   },
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "www.hf.uio.no" },
+      { protocol: "https", hostname: "hf.uio.no" }, // thêm dòng này để Next.js chấp nhận ảnh của bạn
+    ],
   },
 };
 
