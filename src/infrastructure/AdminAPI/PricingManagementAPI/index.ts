c58c9ebc9715cdaa1fc6b62a2964d16d26d5ff7e
Diamond-zero <khoa3349@gmail.com>
2025-10-26 12:10:32 +0700

    Update Pricing Management, update CRUD Price Table Details, add CRUD Price Item Details

--- src/infrastructure/AdminAPI/PricingManagementAPI/index.ts
+++ src/infrastructure/AdminAPI/PricingManagementAPI/index.ts
@@ -1,11 +1,13 @@
 export { PricingService } from './pricingService';
-export { usePriceTables, usePriceItems } from './pricingHooks';
+export { usePriceTables, usePriceItems, usePriceRules } from './pricingHooks';
 export type { 
   PricingData, 
   PriceTablePayload, 
   PricingResponse,
   PriceItem,
   PriceItemResponse,
+  PriceItemPayload,
   PriceRule,
-  PriceRuleResponse
+  PriceRuleResponse,
+  PriceRulePayload
 } from './pricingService';
